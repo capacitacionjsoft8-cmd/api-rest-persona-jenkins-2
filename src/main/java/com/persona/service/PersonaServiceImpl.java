552d9d2fe8568df71102a2ded6b931d98eea2b31
juve <juve@gmail.com>
2026-07-01 08:53:47 -0600

    SUBIENDO PRIMERA VERSION

---
+++ src/main/java/com/persona/service/PersonaServiceImpl.java
@@ -0,0 +1,24 @@
+package com.persona.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.persona.modelo.Persona;
+import com.persona.repository.PersonaRepository;
+
+@Service
+public class PersonaServiceImpl implements PersonaService {
+
+	private final PersonaRepository repository;
+	
+	public PersonaServiceImpl(PersonaRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public List<Persona> listarPersonas() {
+		return repository.findAll();
+	}
+}
